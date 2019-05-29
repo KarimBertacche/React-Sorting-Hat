5f2a6e15ebce20c7240dab54ddadc498dd5e82f9
Karim B <mattiakarim@live.it>
2019-05-29 15:23:41 +0200

    Add questions components for stretch challenge

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const questions = ['Choose a color?', 
+                  'Choose an animal?', 
+                  'Choose a trait that represent you'];
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      questions: questions,
+    }
+  }
+
+  initiationHandler = () => {
+      //Display questions -> From display none to display flex
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          WELCOME TO HOGWARTS!
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.initiationHandler}>Initiation</button>
+        <QuestionsContainer questions={this.state.questions} />
       </div>
     );
   }
+}
+
+
+function QuestionsContainer(props) {
+  return(
+    <div>
+      <QuestionMaker question={props.questions}/>
+    </div>
+  );
+
+}
+
+function QuestionMaker(props) {
+  return(
+    <div>
+      <h3>{props.question}</h3>
+      <input 
+        type="text"
+
+      />
+
+    </div>
+  );
 }
 
 export default App;
